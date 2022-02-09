ed7bb962c9db167f3f4bd5806df59e40cc5db0e3
RaphaelHilt <rafhilt@gmail.com>
2022-02-09 10:53:57 +0100

    ajout annee naissance
    ajout annee avec variable temporelle qui es 2022 et qui nous permet de savoir en quel annee nous sommes nés

--- Test_thibault/main.cpp
+++ Test_thibault/main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-    int age;
+    int age,annee_naissance;
     cout << "Hello world!" << endl;
     cout << "Ton age?" << endl;
     cin >> age;
     cout << "Tu as " << age << " ans" << endl;
+    annee_naissance=2022-age;
+    cout<< "tu es ne en " << annee_naissance<< endl;
+
     return 0;
 }
