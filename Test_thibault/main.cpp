bd97abec85a551864a9f1c099d14204cc4345b4c
Thibault Brun <thib.brun@gmail.com>
2022-02-09 10:45:44 +0100

    Code demande age et affiche age
    Programme qui demande l'age de l'utilisateur et l'affiche.

---
+++ Test_thibault/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Hello world!" << endl;
+    cout << "Ton age?" << endl;
+    cin >> age;
+    cout << "Tu as " << age << " ans" << endl;
+    return 0;
+}
